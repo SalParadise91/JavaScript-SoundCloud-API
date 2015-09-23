a92d37ade5803893c7ac4285210ed4b6bee41fcc
evdokimovm <tel89147925532@gmail.com>
2015-09-23 21:33:16 +1000

    Authentication

--- script.js
+++ script.js
@@ -2,14 +2,30 @@
 //   client_id: '47159083054685525f6b73d25e2560b9'
 // });
 
+// SC.initialize({
+//   client_id: '47159083054685525f6b73d25e2560b9'
+// });
+
+// $(document).ready(function() {
+//   SC.get('/tracks', { genres: 'foo' }, function(tracks) {
+//      $(tracks).each(function(index, track) {
+//        $('#results').append($('<li></li>').html(track.title + ' - ' + track.genre));
+//      });
+//   });
+// });
+
 SC.initialize({
-  client_id: '47159083054685525f6b73d25e2560b9'
+  client_id: '47159083054685525f6b73d25e2560b9',
+  redirect_uri: 'http://31.200.238.29/SoundCloud/'
 });
 
 $(document).ready(function() {
-  SC.get('/tracks', { genres: 'foo' }, function(tracks) {
-     $(tracks).each(function(index, track) {
-       $('#results').append($('<li></li>').html(track.title + ' - ' + track.genre));
-     });
+  $('a.connect').click(function(e) {
+    e.preventDefault();
+    SC.connect(function() {
+      SC.get('/me', function(me) {
+        $('#username').html(me.username);
+      });
+    });
   });
 });
