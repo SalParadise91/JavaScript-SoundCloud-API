735d96368550d8428ecb835abdd0c3bc2a7d0dad
evdokimovm <tel89147925532@gmail.com>
2015-09-23 21:24:37 +1000

    Making Requests

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// SC.initialize({
+//   client_id: '47159083054685525f6b73d25e2560b9'
+// });
+
 SC.initialize({
   client_id: '47159083054685525f6b73d25e2560b9'
+});
+
+$(document).ready(function() {
+  SC.get('/tracks', { genres: 'foo' }, function(tracks) {
+     $(tracks).each(function(index, track) {
+       $('#results').append($('<li></li>').html(track.title + ' - ' + track.genre));
+     });
+  });
 });
