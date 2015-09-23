8977c7cae068ca3c2e30d0c2c25953cf08399c25
evdokimovm <tel89147925532@gmail.com>
2015-09-23 21:42:14 +1000

    Fetch a Sound to Play

--- script.js
+++ script.js
@@ -30,10 +30,20 @@
 //   });
 // });
 
+// SC.initialize({
+//   client_id: '47159083054685525f6b73d25e2560b9'
+// });
+
+// $(document).ready(function() {
+
+// });
+
 SC.initialize({
-  client_id: '47159083054685525f6b73d25e2560b9'
+    client_id: '47159083054685525f6b73d25e2560b9'
 });
 
 $(document).ready(function() {
-
+    SC.get('/tracks/293',function(track){ 
+        $('#player').html(track.title);
+    });
 });
